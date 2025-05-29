54f60164825222b3a65ddd89da8b59173e4143d5
Ben1524 <wy17674094293@163.com>
2025-05-29 22:47:06 +0800

    feature:增加未完成的双向队列接口

---
+++ common/deque.h
@@ -0,0 +1,22 @@
+//
+// Created by cxk_zjq on 25-5-29.
+//
+
+#ifndef DEQUE_H
+#define DEQUE_H
+
+#pragma once
+
+#include <deque>
+
+namespace cxk
+{
+    template <typename T, typename Alloc = std::allocator<T>>
+    using Deque = std::deque<T, Alloc>;
+
+    // TODO: 实现多读一写线程安全的deque
+
+}
+
+
+#endif //DEQUE_H
